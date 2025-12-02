5a166ee77c5b5eb51a23ff8db7e03139101a06bd
phong <phong@gmail.com>
2025-12-02 10:25:54 +0700

    Add

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:3001";
 
-export const API_URL = "http://localhost:3001";
+export const API_URL = "https://ia06.onrender.com";
 
 export async function registerUser({ email, password }) {
   // trả về data JSON của API
-  const res = await axios.post(`${API_BASE}/user/register`, { email, password });
+  const res = await axios.post(`${API_URL}/user/register`, { email, password });
   return res.data;
 };
 
